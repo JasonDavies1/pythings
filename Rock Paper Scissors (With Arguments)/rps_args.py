eb1f453503db90fd0b2085d151f5ad7634768415
Jason Davies <jason.davies@healthcaregateway.co.uk>
2019-04-27 18:44:14 +0100

    Made small functions in script for looping

--- Rock Paper Scissors (With Arguments)/rps_args.py
+++ Rock Paper Scissors (With Arguments)/rps_args.py
@@ -6,47 +6,61 @@ inputs = sys.argv[1::]
 
 number_of_inputs = len(inputs)
 
-if number_of_inputs == 1 : 
-    print("Please use more than one argument")
-    sys.exit()
-elif number_of_inputs % 2 == 0 :
-    print("Invalid number of inputs, retry with an odd number")
-    sys.exit()
+def check_arguments_correct(n) :
+    if number_of_inputs == 1 : 
+        print("Please use more than one argument")
+        sys.exit()
+    elif number_of_inputs % 2 == 0 :
+        print("Invalid number of inputs, retry with an odd number")
+        sys.exit()
 
+def generateCombonations(inputs, wins_losses) :
+    combonations = defaultdict(list)
+
+    for index, item in enumerate(inputs) : 
+        for j in range(1, wins_and_losses) : 
+            combonations[inputs[index]].append(inputs[(index + j) % number_of_inputs])
+    return combonations
+
+def display_wins_losses_dictionary(dic) :
+    for key, value in combonations.items() :
+        print(key + " beats:")
+        for item in value :
+            print(item + " ")
+        print()
+
+def get_player_name(player_number) :
+    return input("What is Player " + str(player_number) + "'s name?: ")
+
+def get_player_choice(name, inputs):
+    player_choice = getpass("Make your choice " + name + ": ") 
+    while player_choice not in inputs :
+        print("Get it together " + name + " and choose something in the list!")
+        player_choice = getpass("Make your choice " + name + ": ") 
+    return player_choice
+
+def assert_winner(p1_name, p2_name, p1_choice, p2_choice, p1_results) :
+    if p1_choice == p2_choice :
+        print("Tie game!")
+        sys.exit()
+    elif p2_choice in p1_results :
+        print(p1_name + " wins!")
+        sys.exit()
+    else :
+        print(p2_name + " wins!")
+
+#setup
+check_arguments_correct(number_of_inputs)
 wins_and_losses = int((number_of_inputs + 1) / 2)
-combonations = defaultdict(list)
+combonations = generateCombonations(inputs, wins_and_losses)
+display_wins_losses_dictionary(combonations)
+player_one_name = get_player_name(1)
+player_two_name = get_player_name(2)
 
-for index, item in enumerate(inputs) : 
-    for j in range(1, wins_and_losses) : 
-        combonations[inputs[index]].append(inputs[(index + j) % number_of_inputs])
-
-for key, value in combonations.items() :
-    print(key + " beats:")
-    for item in value :
-        print(item + " ")
-    print()
-
-player_one_name = input("What is Player 1's name?: ")
-player_two_name = input("What is Player 2's name?: ") 
-
-player_one_choice = getpass("Make your choice " + player_one_name + ": ") 
-while player_one_choice not in inputs :
-    print("Get it together " + player_one_name + " and choose something in the list!")
-    player_one_choice = getpass("Make your choice " + player_one_name + ": ") 
-
-player_two_choice = getpass("Make your choice " + player_two_name + ": ") 
-while player_two_choice not in inputs : 
-    print("Get it together " + player_two_name + " and choose something in the list!")
-    player_two_choice = getpass("Make your choice " + player_two_name + ": ")
+#this should loop
+player_one_choice = get_player_choice(player_one_name, inputs)
+player_two_choice = get_player_choice(player_two_name, inputs)
 
 player_one_results = combonations[player_one_choice]
-player_two_results = combonations[player_two_choice]
 
-if player_one_choice == player_two_choice :
-    print("Tie game!")
-    sys.exit()
-elif player_two_choice in player_one_results :
-    print(player_one_name + " wins!")
-    sys.exit()
-else :
-    print(player_two_name + " wins!")
+assert_winner(player_one_name, player_two_name, player_one_choice, player_two_choice, player_one_results)
