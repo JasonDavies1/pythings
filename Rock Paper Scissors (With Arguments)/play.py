3c9f32ab85568a00b32b0181a4c6f66d78338cad
Jason Davies <jason.davies@healthcaregateway.co.uk>
2019-04-29 20:10:02 +0100

    Readibility improvements and typo corrections

--- Rock Paper Scissors (With Arguments)/play.py
+++ Rock Paper Scissors (With Arguments)/play.py
@@ -6,7 +6,8 @@ number_of_inputs = len(inputs)
 
 def play_again() :
     selection = input("Play Again? (Yes/No): ")
-    while selection not in ["Yes", "No"] :
+    options = ["Yes", "No"] 
+    while selection not in options :
         print("Please try again...")
         selection = input("Play Again? (Yes/No): ")
     return selection
@@ -14,8 +15,8 @@ def play_again() :
 #setup
 setup.check_arguments_correct(number_of_inputs)
 wins_and_losses = int((number_of_inputs + 1) / 2)
-combonations = setup.generateCombonations(inputs, wins_and_losses, number_of_inputs)
-setup.display_wins_losses_dictionary(combonations)
+combinations = setup.generate_combinations(inputs, wins_and_losses, number_of_inputs)
+setup.display_wins_losses_dictionary(combinations)
 player_one_name = setup.get_player_name(1)
 player_two_name = setup.get_player_name(2)
 
@@ -26,7 +27,7 @@ while replay != 'No' :
     player_one_choice = setup.get_player_choice(player_one_name, inputs)
     player_two_choice = setup.get_player_choice(player_two_name, inputs)
 
-    player_one_results = combonations[player_one_choice]
+    player_one_results = combinations[player_one_choice]
 
     print(setup.get_result(player_one_name, player_two_name, player_one_choice, player_two_choice, player_one_results))
     replay = play_again()
--- Rock Paper Scissors (With Arguments)/rps_setup.py
+++ Rock Paper Scissors (With Arguments)/rps_setup.py
@@ -10,12 +10,12 @@ def check_arguments_correct(n) :
         print("Invalid number of inputs, retry with an odd number")
         sys.exit()
 
-def generateCombonations(inputs, wins_losses, number_of_inputs) :
-    combonations = defaultdict(list)
+def generate_combinations(inputs, wins_losses, number_of_inputs) :
+    combinations = defaultdict(list)
     for index, item in enumerate(inputs) : 
         for j in range(1, wins_losses) : 
-            combonations[inputs[index]].append(inputs[(index + j) % number_of_inputs])
-    return combonations
+            combinations[inputs[index]].append(inputs[(index + j) % number_of_inputs])
+    return combinations
 
 def display_wins_losses_dictionary(dic) :
     for key, value in dic.items() :
